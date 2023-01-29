3e3ad06f5f99679eb88a805b00d5b0c7ee01e6ca
Lukas1h <Lukas1h7@gmail.com>
2023-01-29 14:45:17 -0800

    This is the part where i screw the islands up

--- src/index2.js
+++ src/index2.js
@@ -15,7 +15,7 @@ var url_string = (window.location.href).toLowerCase();
 var url = new URL(url_string);
 var id = url.searchParams.get("id");
 if(id != null){
-    console.log("id is" + id)
+    // console.log("id is" + id)
 }else{
     alert("invalid id")
 }
@@ -23,7 +23,7 @@ var player = url.searchParams.get("p");
 if(player == null || (player != "1" && player != "2")){
     alert("invalid player")
 }else{
-    console.log("id is" + id)
+    // console.log("id is" + id)
 }
 
 
@@ -40,6 +40,9 @@ document.onmousemove = function(event){
  cursor_y = event.pageY;
 }
 
+var xPan = 0
+var yPan = 0
+
 
 var frame = {
   start: null,
@@ -50,7 +53,7 @@ var frame = {
 const INTERVAL = 16;
 
 
-console.log('games/'+id)
+// console.log('games/'+id)
 var gameRef = firebase.database().ref('games/'+id);
 
 
@@ -98,12 +101,12 @@ var placeType = "plane"
 
 
 document.getElementById("plane").addEventListener("click",()=>{
-    console.log("place type plane")
+    // console.log("place type plane")
     placeType = "plane"
 })
 
 document.getElementById("ship").addEventListener("click",()=>{
-    console.log("place type ship")
+    // console.log("place type ship")
     placeType = "ship"
 })
 
@@ -111,13 +114,13 @@ document.getElementById("ship").addEventListener("click",()=>{
 gameRef.on('value', (snapshot) => {
 
   const data = snapshot.val();
-  console.log(data)
-  console.log(JSON.parse(data))
+//   console.log(data)
+//   console.log(JSON.parse(data))
 
 
 
   if(JSON.parse(data) == null){
-    console.log('creating new room')
+    // console.log('creating new room')
   }else{
     game = JSON.parse(data)
   }
@@ -145,72 +148,70 @@ var debug = false
 
 groundImg.onload = () => { // Only use the image after it's loaded
   pattern = ctx.createPattern(groundImg, "repeat");
-  render()
 };
 
 
-canvas.addEventListener("click",(e)=>{
-    if(debug){
-        alert("Trying to place piece")
-    }
-    alert("screen shot this, " + cursor_x + "x" + (cursor_y-50))
-    if(mode == "place"){
-        if(placeType == "plane"){
-            if(isPointOnSide(cursor_x,cursor_y-50)){
-                if(debug){
-                    alert("Placing Plane")
-                }
-                game.players[player-1].pieces.push({
-                    pos:{
-                        x:cursor_x,
-                        y:cursor_y-50,
-                    },
-                    type:placeType
-                })
-            }else{
-                if(debug){
-                    alert("Cant place. Not on side.")
-                }
-            }
-        }
+// canvas.addEventListener("click",(e)=>{
+//     if(debug){
+//         alert("Trying to place piece")
+//     }
+//     alert("screen shot this, " + cursor_x + "x" + (cursor_y-50))
+//     if(mode == "place"){
+//         if(placeType == "plane"){
+//             if(isPointOnSide(cursor_x,cursor_y-50)){
+//                 if(debug){
+//                     alert("Placing Plane")
+//                 }
+//                 game.players[player-1].pieces.push({
+//                     pos:{
+//                         x:cursor_x,
+//                         y:cursor_y-50,
+//                     },
+//                     type:placeType
+//                 })
+//             }else{
+//                 if(debug){
+//                     alert("Cant place. Not on side.")
+//                 }
+//             }
+//         }
 
-        if(placeType == "ship"){
-            if(!isPointOnLand(cursor_x,cursor_y-50)){
-                alert("Trying to place boat on water.")
-                if(isPointOnSide(cursor_x,cursor_y-50)){
-                    alert("Placing Boat")
-                    game.players[player-1].pieces.push({
-                    pos:{
-                        x:cursor_x,
-                        y:cursor_y-50,
-                    },
-                    type:placeType
-                })
-                }else{
-                    if(debug){
-                        alert("Cant place. Not on side.")
-                    }
-                }
-            }else{
-                if(debug){
-                    alert("Cant place. Not on land.")
-                }
-            }
+//         if(placeType == "ship"){
+//             if(!isPointOnLand(cursor_x,cursor_y-50)){
+//                 alert("Trying to place boat on water.")
+//                 if(isPointOnSide(cursor_x,cursor_y-50)){
+//                     alert("Placing Boat")
+//                     game.players[player-1].pieces.push({
+//                     pos:{
+//                         x:cursor_x,
+//                         y:cursor_y-50,
+//                     },
+//                     type:placeType
+//                 })
+//                 }else{
+//                     if(debug){
+//                         alert("Cant place. Not on side.")
+//                     }
+//                 }
+//             }else{
+//                 if(debug){
+//                     alert("Cant place. Not on land.")
+//                 }
+//             }
 
-        }
-
-
-        gameRef.set(JSON.stringify(game))
-        render()
-    }else{
-        console.log(mode)
-    }
-})
+//         }
 
 
+//         gameRef.set(JSON.stringify(game))
+//         render()
+//     }else{
+//         console.log(mode)
+//     }
+// })
 
 
 function render(){
+    ctx.translate(-xPan,-yPan)
     ctx.clearRect(0, 0, canvas.width, canvas.height); 
 
 
@@ -221,12 +222,13 @@ function render(){
 
     for(var playerNum = 1; playerNum<=2;playerNum++){
         game.players[playerNum-1].pieces.forEach((piece,index)=>{
-            console.log("drawing, ", piece.type)
+            // console.log("drawing, ", piece.type)
             drawPieceAt(piece.pos.x,piece.pos.y,playerNum,piece.type)
             
         })
     }
 
+    ctx.translate(xPan,yPan)
 }
 
 function isPointOnSide(px,py){
@@ -280,8 +282,6 @@ function checkPoint(x, y, curves, canvasColor, strokeStyle) {
 }
 
 
-
-
 function drawCurve(x, y, curves, canvasColor, strokeStyle) {
   ctx.beginPath();
   ctx.moveTo(x, y);
@@ -312,9 +312,6 @@ function drawCurve2(x, y, curves, canvasColor, strokeStyle) {
 }
 
 
-
-
-
 function drawIslandP1(){
 
   let start = game.islands[0].start
@@ -323,7 +320,7 @@ function drawIslandP1(){
   let end =   game.islands[0].end
 
 
-  console.log(start,cp1,cp2,end)
+//   console.log(start,cp1,cp2,end)
     drawCurve(
         start.x,
         start.y, [
@@ -334,9 +331,9 @@ function drawIslandP1(){
     );
 }
 
-
-
 function drawIslandP2(){
+    
+
     let start = game.islands[1].start
     let cp1 =   game.islands[1].cp1
     let cp2 =   game.islands[1].cp2
@@ -358,18 +355,20 @@ function drawIslandP2(){
 }
 
 
-
 function renderUI(){
     ctx.font = "20px Arial";
     ctx.fillText(player == 1 ? "Player One" : "Player Two", 10, 20);
 }
 
 
-function drawPieceAt(xOffset, yOffset,p,typee) {
+function drawPieceAt(xOffsetOG, yOffsetOG,p,typee) {
 
+
+    xOffset = xOffsetOG + xPan
+    yOffset = yOffsetOG + yPan
     if(p == player){
         if(typee == "plane"){
-            console.log("type is ",typee)
+            // console.log("type is ",typee)
             drawImage(ctx,planeImg,xOffset-25,yOffset-25,50,50,0);
         }
         if(typee == "ship"){
@@ -378,7 +377,7 @@ function drawPieceAt(xOffset, yOffset,p,typee) {
     }else{
         var x = canvas.width - xOffset
         var y = canvas.height - yOffset
-        console.log("type is ",typee)
+        // console.log("type is ",typee)
         if(typee == "plane"){
             drawImage(ctx,planeImg,canvas.width-xOffset-25,canvas.height-yOffset-25,50,50,180);
         }
@@ -393,9 +392,6 @@ function drawPieceAt(xOffset, yOffset,p,typee) {
 }
 
 
-
-
-
 function drawImage(ctx, image, x, y, w, h, degrees){
   ctx.save();
   ctx.translate(x+w/2, y+h/2);
@@ -404,3 +400,165 @@ function drawImage(ctx, image, x, y, w, h, degrees){
   ctx.drawImage(image, x, y, w, h);
   ctx.restore();
 }
+
+
+
+
+// function run(func) {
+//   if (!frame.start) frame.start = performance.now();
+//   frame.delta = performance.now() - frame.start;
+//   if (frame.delta >= INTERVAL) {
+//     func.call();
+//     frame.start = null; //reset
+//     frame.count++;
+//   }
+//   //for this example the loop will be repeated only 100 time
+//   requestAnimationFrame(run.bind(null, func));
+//   //without counting limit
+//   //requestAnimationFrame(run.bind(null, func));
+// }
+
+
+// run(render)
+
+
+let cameraOffset = { x: window.innerWidth/2, y: window.innerHeight/2 }
+let cameraZoom = 1
+let MAX_ZOOM = 5
+let MIN_ZOOM = 0.1
+let SCROLL_SENSITIVITY = 0.0005
+
+function draw()
+{
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    
+    // Translate to the canvas centre before zooming - so you'll always zoom on what you're looking directly at
+    ctx.translate( window.innerWidth / 2, window.innerHeight / 2 )
+    ctx.scale(cameraZoom, cameraZoom)
+    ctx.translate( -window.innerWidth / 2 + cameraOffset.x, -window.innerHeight / 2 + cameraOffset.y )
+    ctx.clearRect(0,0, window.innerWidth, window.innerHeight)
+    render()
+    
+    requestAnimationFrame( draw )
+}
+
+// Gets the relevant location from a mouse or single touch event
+function getEventLocation(e)
+{
+    if (e.touches && e.touches.length == 1)
+    {
+        return { x:e.touches[0].clientX, y: e.touches[0].clientY }
+    }
+    else if (e.clientX && e.clientY)
+    {
+        return { x: e.clientX, y: e.clientY }        
+    }
+}
+
+function drawRect(x, y, width, height)
+{
+    ctx.fillRect( x, y, width, height )
+}
+
+function drawText(text, x, y, size, font)
+{
+    ctx.font = `${size}px ${font}`
+    ctx.fillText(text, x, y)
+}
+
+
+let isDragging = false
+let dragStart = { x: 0, y: 0 }
+
+function onPointerDown(e)
+{
+    isDragging = true
+    dragStart.x = getEventLocation(e).x/cameraZoom - cameraOffset.x
+    dragStart.y = getEventLocation(e).y/cameraZoom - cameraOffset.y
+}
+
+function onPointerUp(e)
+{
+    isDragging = false
+    initialPinchDistance = null
+    lastZoom = cameraZoom
+}
+
+function onPointerMove(e)
+{
+    if (isDragging)
+    {
+        cameraOffset.x = getEventLocation(e).x/cameraZoom - dragStart.x
+        cameraOffset.y = getEventLocation(e).y/cameraZoom - dragStart.y
+    }
+}
+
+function handleTouch(e, singleTouchHandler)
+{
+    if ( e.touches.length == 1 )
+    {
+        singleTouchHandler(e)
+    }
+    else if (e.type == "touchmove" && e.touches.length == 2)
+    {
+        isDragging = false
+        handlePinch(e)
+    }
+}
+
+let initialPinchDistance = null
+let lastZoom = cameraZoom
+
+function handlePinch(e)
+{
+    e.preventDefault()
+    
+    let touch1 = { x: e.touches[0].clientX, y: e.touches[0].clientY }
+    let touch2 = { x: e.touches[1].clientX, y: e.touches[1].clientY }
+    
+    // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
+    let currentDistance = (touch1.x - touch2.x)**2 + (touch1.y - touch2.y)**2
+    
+    if (initialPinchDistance == null)
+    {
+        initialPinchDistance = currentDistance
+    }
+    else
+    {
+        adjustZoom( null, currentDistance/initialPinchDistance )
+    }
+}
+
+function adjustZoom(zoomAmount, zoomFactor)
+{
+    if (!isDragging)
+    {
+        if (zoomAmount)
+        {
+            cameraZoom += zoomAmount
+        }
+        else if (zoomFactor)
+        {
+            console.log(zoomFactor)
+            cameraZoom = zoomFactor*lastZoom
+        }
+        
+        cameraZoom = Math.min( cameraZoom, MAX_ZOOM )
+        cameraZoom = Math.max( cameraZoom, MIN_ZOOM )
+        
+        console.log(zoomAmount)
+    }
+}
+
+canvas.addEventListener('mousedown', onPointerDown)
+canvas.addEventListener('touchstart', (e) => handleTouch(e, onPointerDown))
+canvas.addEventListener('mouseup', onPointerUp)
+canvas.addEventListener('touchend',  (e) => handleTouch(e, onPointerUp))
+canvas.addEventListener('mousemove', onPointerMove)
+canvas.addEventListener('touchmove', (e) => handleTouch(e, onPointerMove))
+canvas.addEventListener( 'wheel', (e) => adjustZoom(e.deltaY*SCROLL_SENSITIVITY))
+
+// Ready, set, go
+draw()
+
